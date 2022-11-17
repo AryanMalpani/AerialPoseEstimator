db52a0610bf642bfc41db0e4f4025a1de1078090
AryanMalpani <aryanmalpani02@gmail.com>
2022-11-17 13:47:00 +0530

    Update: Clouding files for running crossloc

---
+++ clouding.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import os
+
+def gammaCorrection(src, filename):
+    path = "/home/aryna/Documents/Sem 5/CV/CrossLoc/datasets/naturescape/test_drone_real/rgb/"
+#     direc = str(filename)+"/"
+#     path = os.path.join(parent, direc) 
+#     os.mkdir(path)
+    gammaMatrix = [1]
+    for gamma in gammaMatrix:
+        invGamma = 1 / gamma
+
+        table = [((i / 255) ** invGamma) * 255 for i in range(256)]
+        table = np.array(table, np.uint8)
+        file = path+str(filename)
+        cv2.imwrite(file, cv2.LUT(src, table))
+
+folder = "/home/aryna/Documents/Sem 5/CV/CrossLoc/datasets/naturescape/test_drone_real/rgb_og/"
+# count = 0
+
+for filename in os.listdir(folder):
+    # count += 1
+    temp = os.path.join(folder,filename)
+    if(temp.endswith('png')):
+        img = cv2.imread(temp)
+        gammaCorrection(img, filename)
+    # print(count)
